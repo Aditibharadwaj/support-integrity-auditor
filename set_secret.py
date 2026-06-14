e0e0b6f362d8da51dfa25517a65eb426c769df2b
Aditi Bharadwaj <aditibr1105@gmail.com>
2026-06-14 17:10:30 +0530

    Remove set_secret.py

--- set_secret.py
+++
@@ -1,7 +0,0 @@
-from huggingface_hub import HfApi
-api = HfApi()
-api.add_space_secret(
-    repo_id="aditibharadwaj/support-integrity-auditor",  # your actual Space name
-    key="MISTRAL_API_KEY",
-    value="OT13NNHFIORFqDcajjtbysA3PAMptO9W",
-)
